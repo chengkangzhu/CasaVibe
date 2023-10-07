530e8e31af5b946a86225d18cc219eb4586f32dc
cheng <y2984491@gmail.com>
2023-10-07 01:46:02 -0400

    pdp variation change

--- frontend/src/pages/ProductDetail.jsx
+++ frontend/src/pages/ProductDetail.jsx
@@ -806,8 +806,9 @@ const ProductDetail = () => {
 	//show contextimage on hover
 	const [isDisplayHovered, setIsDisplayHovered] = useState(false);
 
+
 	//set selected variation to have outline 
-	const [variation, setVariation] = useState(0)
+	const [variation, setVariation] = useState(-1)
 
 	//list of products display on shopgrid 
 	const {id} = useParams()	  
@@ -815,6 +816,10 @@ const ProductDetail = () => {
 	//get the product data from redux store 
 	const productObj = useSelector((state)=>state.products.pdp)
  
+	//main product
+	const [currrentProduct, setCurrentProduct] = useState(productObj)
+
+	//double check the id with the product 
 	useEffect(()=>{    
 		console.log('ehy')
 	},[id])
@@ -827,21 +832,21 @@ const ProductDetail = () => {
 
 					{/* LEFT */}
 					<div className="img_container" onMouseOver={() => setIsDisplayHovered(true)} onMouseOut={() => setIsDisplayHovered(false)}>  
-						<img className={`main ${isDisplayHovered && "hidden"}`} src={productObj.image} alt={productObj.imageAlt} />
-        				<img className={`contextImg ${isDisplayHovered && "show"}`} src={productObj.contextualImageUrl} alt={productObj.imageAlt} />		
+						<img className={`main ${isDisplayHovered && "hidden"}`} src={currrentProduct.image} alt={currrentProduct.imageAlt} />
+        				<img className={`contextImg ${isDisplayHovered && "show"}`} src={currrentProduct.contextualImageUrl} alt={currrentProduct.imageAlt} />		
 					</div>
 
 					{/* RIGHT */}
 					<div className="product_info"> 
-						<h2 className="h2 sb product_name">{productObj.name}</h2>
-						<p className="h7 rg">{productObj.imageAlt}</p>
+						<h2 className="h2 sb product_name">{currrentProduct.currrentProductname}</h2>
+						<p className="h7 rg">{currrentProduct.imageAlt}</p>
 						<div className="price_rating_container">
 							<div className="price_container">
-								{productObj.price.discounted && 
+								{currrentProduct.price.discounted && 
 								(<div><span className="discount_percent h8 md">20%</span>
-								<span className="original_price tiny_text">${parseFloat((productObj.price.currentPrice * 1.25).toFixed(2))} </span></div>)
+								<span className="original_price tiny_text">${parseFloat((currrentProduct.price.currentPrice * 1.25).toFixed(2))} </span></div>)
 								}
-								<h2 className="h2 sb current_price">${productObj.price.currentPrice}</h2>
+								<h2 className="h2 sb current_price">${currrentProduct.price.currentPrice}</h2>
 							</div>
 							<div className="review_container">
 								<Rating rating={4.5} reviews="535"/>
@@ -853,17 +858,17 @@ const ProductDetail = () => {
 								<div className="variation_images">
 
 								{/* outline on first default variation */}
-								<img src={productObj.image} alt={productObj.imageAlt}  className={variation === 0 && "shape_outline_active"}  onClick={()=> setVariation(0)}/>
+								<img src={productObj.image} alt={productObj.imageAlt}  className={variation === -1 && "shape_outline_active"}  onClick={()=> {setVariation(-1);setCurrentProduct(productObj)}}/>
 
 								{/* when clicked will change the variant state nubmer to index+1, show outline*/}
 								{productObj.variants  && productObj.variants.map((variant, index)=>{ 
-									return <img key={index} src={variant.image} alt={variant.imageAlt} className={variation === index+1 && "shape_outline_active"} onClick={()=> setVariation(index + 1)}/> 
+									return <img key={index} src={variant.image} alt={variant.imageAlt} className={variation === index && "shape_outline_active"} onClick={()=>{setVariation(index);setCurrentProduct(productObj.variants[index])}}/> 
 								}) }
 							</div>
 						</div>
 						<div className="action_container">
 							<div className="stocks_container">
-								<h5 className="h5 rg">Stock: <span className="md">{parseInt(productObj.id.slice(-3)) } left</span></h5>
+								<h5 className="h5 rg">Stock: <span className="md">{parseInt(currrentProduct.id.slice(-3)) } left</span></h5>
 								<QuantitySelector amount={2} weight="150px" height="56px" className="h6" iconSize={24}/>
 							</div>
 							<div className="add_item_container">
