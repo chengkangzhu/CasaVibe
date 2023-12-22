8a9f8734f5672f56171a2c5e905681f4d7440254
Cheng Kang <y2984491@gmail.com>
2023-12-22 13:50:42 -0400

    added 404 page

--- frontend/src/pages/ProductDetail.jsx
+++ frontend/src/pages/ProductDetail.jsx
@@ -143,7 +143,7 @@ const ProductDetail = () => {
 		} else {
 			fetchDataById(id);
 		}
-	}, [id,currentProduct, fetchDataById, fetchRelatedItems, reduxPdp]);
+	}, [id]);
 
 	const handleLike = () => {
 		//update the isLiked state
